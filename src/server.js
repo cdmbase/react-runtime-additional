f9102f0ae41c0e19ebece1598f82c1383185eaa6
veeramarni <mvasireddy@cdmbase.com>
2016-11-19 19:33:22 -0500

    add logs dir

--- src/server.js
+++ src/server.js
@@ -1,36 +1,35 @@
 var bunyan = require('bunyan');
 var isMeteor = require('./util');
 
-var stream, level, type;
+var  level, type, streams;
 
 const app = process.env.APP_NAME || 'app';
 if (process.env.NODE_ENV === "production") {
     //TODO: Get the  settings from Meteor.settings or node environment
     console.log("It is production");
 
-    type = 'raw';
     level =  isMeteor() && Meteor.settings.public.logLevel || process.env.logLevel || 'info';
-    stream = {
-        type: 'rotation-file',
-        path: `/var/log/${app}.log`,
+    streams = [{
+        type: 'rotating-file',
+        path: `logs/${app}.log`,
         period: '1d',   // daily rotation
         count: 3        // keep 3 back copies    }
-    }
+    }]
 
 } else {
     //stream = new PrettyStream(process.stdout);
     //type = 'raw';
     type = 'stream';
-    stream = process.stdout;
-    level =  isMeteor() && Meteor.settings.public.logLevel || process.env.logLevel || 'debug'
+    level =  isMeteor() && Meteor.settings.public.logLevel || process.env.logLevel || 'debug';
+    streams: [{
+        level: level,
+        type: 'stream',
+        stream: process.stdout,
+    }]
 }
 const logger = bunyan.createLogger({
         name: app,
-        streams: [{
-            level: level,
-            type: type,
-            stream: stream,
-        }]
+        streams: streams
     }
 )
 
